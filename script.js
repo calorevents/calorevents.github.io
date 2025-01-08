3a9d2852f6b49931d8025dfd27dc05fedaf676e1
Edgar van Amerongen <edgar@Edgars-Mac-mini.local>
2025-01-08 17:10:33 +0100

    new stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+/*
+function openTab(tabName) {
+    // Hide all tab contents
+    var tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(function (content) {
+      content.style.display = 'none';
+    });
+  
+    // Show the selected tab content
+    document.getElementById(tabName).style.display = 'block';
+  }
+  */
